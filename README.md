d957db039bf555960aa0f12441b8976005008ac1
jxp-home <424772147@qq.com>
2018-03-08 13:33:46 +0800

    complete config server and client

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 1.创建配置文件存放仓库repo
 2.创建一个配置文件服务器server
 http://localhost:8888/cloud-config/test/master可验证Config服务器从Git存储库读到了配置文件
+获取git上的资源信息遵循如下规则：
+/{application}/{profile}[/{label}]
+/{application}-{profile}.yml
+/{label}/{application}-{profile}.yml
+/{application}-{profile}.properties
+/{label}/{application}-{profile}.properties
 3.创建一个应用服务client使用分布式配置文件信息
--- cloud-config-client/src/main/java/com/jxp/MySampleRestController.java
+++ cloud-config-client/src/main/java/com/jxp/MySampleRestController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope //注解@RefreshScope指示Config客户端在服务器参数刷新时，也刷新注入的属性值
 public class MySampleRestController {
 	@Value("${mysqldb.datasource.url}")
-	private String appName;
+	private String value;
 	
-	@RequestMapping("/app-name")
-	public String getAppName() {
-		return appName;
+	@RequestMapping("/value")
+	public String getValue() {
+		return value;
 	}
 }
